afe0ca981689c5c784ae373ac681ad8acb7279ae
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-09-21 18:36:04 +0100

    added loading images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from "lodash";
 import "./style.css";
+import Icon from "./icon.png";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["hello", "webpack"], " ");
   element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
   return element;
 }
 
