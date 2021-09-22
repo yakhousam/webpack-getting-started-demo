b531f2348135ea688d6d9e63d0b2cac5fcc6b196
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-09-22 11:10:21 +0100

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-import Data from "./data.xml";
-import Notes from "./data.csv";
-import toml from "./data.toml";
-import yaml from "./data.yaml";
-import json5 from "./data.json5";
-
-console.log(toml.title);
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json5.title);
-console.log(json5.owner.name);
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   element.innerHTML = _.join(["hello", "webpack"], " ");
-  element.classList.add("hello");
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = "Click me and check the console";
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
+  element.appendChild(btn);
 
   return element;
 }
