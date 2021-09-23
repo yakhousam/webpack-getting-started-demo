b5a971f609c0cf4991df856745af6612b4df4360
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-09-23 16:13:42 +0100

    Development

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: "./src/index.js",
     pring: "./src/print.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "Development",
     }),
   ],
   output: {
